90e252942bf060bff8e38f69788076156d981d52
AlaaElrasheed <wethag.234@gmail.com>
2023-09-20 13:42:25 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strncpy - The main function
+ *
+ *@src: Function value
+ *
+ * @n: Function value
+ *
+ * @dest: Function value
+ *
+ * Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l = 0;
+
+	while (l < n && src[l] != '\0')
+	{
+		dest[l] = src[l];
+		l++;
+	}
+	while (l < n)
+	{
+		dest[l] = '\0';
+		l++;
+	}
+		return (dest);
+}
+
