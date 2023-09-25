41ce8e402c83f2845638ab9546ce9b453f8632fe
AlaaElrasheed <wethag.234@gmail.com>
2023-09-25 07:12:40 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - Entrey point
+  *
+  * @s: input deal with the function
+  *
+  * @to: input deal with the function
+  *
+  * Return: Always 0
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
