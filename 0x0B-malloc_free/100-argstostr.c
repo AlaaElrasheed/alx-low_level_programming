ff7e0ca26003d0955491ef694e8a51fde47d6aef
AlaaElrasheed <wethag.234@gmail.com>
2023-10-04 03:37:43 -0700

    6

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * argstostr - Entry point
+ *
+ * @ac: function input
+ *
+ * @av: function input
+ *
+ * Return: 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int d = 0;
+	int total = 0;
+	char *r;
+
+	if (ac <= 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		total += strlen(av[i]) + 1;
+	}
+
+	r = malloc(sizeof(r) * total);
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(r + d, av[i]);
+		d += strlen(av[i]);
+		r[d++] = '\n';
+	}
+
+	r[d] = '\0';
+	return (r);
+}
