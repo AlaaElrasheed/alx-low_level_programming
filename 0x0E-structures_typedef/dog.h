2205acf1ca10b1d57080faa82489bea7bf466a90
AlaaElrasheed <wethag.234@gmail.com>
2023-10-10 07:38:59 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ *
+ * @name: dog's name
+ *
+ * @owner: owner's name
+ *
+ * @age: dog's age
+ *
+ * Description: A lonley dog in kitty's universe
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
