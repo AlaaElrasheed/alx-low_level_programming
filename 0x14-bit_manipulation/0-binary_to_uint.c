ef72644f6a40261053ecb91ad4aeec573132544f
AlaaElrasheed <wethag.234@gmail.com>
2023-10-26 04:53:35 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int x = 0, y = 0;
-	unsigned int count;
+	int x = 0;
+	unsigned int count = 0;
 
 	if (!b)
 		return (0);
-	while (b[x] != '\0')
+	for (x = 0; b[x] != '\0'; x++)
 	{
 		if (b[x] != '0' && b[x] != '1')
 			return (0);
-		x++;
 	}
-	x--;
-	while (x >= 0)
+	for (x = 0; b[x] != '\0'; x++)
 	{
-		count += (b[x] - '0') * (1 << y);
-		x--;
-		y++;
+		count <<= 1;
+		if (b[x] == '1')
+			count += 1;
 	}
 	return (count);
 }
