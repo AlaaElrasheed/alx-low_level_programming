277817a02bbbe092557ce5bdf409a3a801dea979
Alaa Elrasheed <wethag.234@gmail.com>
2023-11-23 04:01:53 -0800

    3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,68 +1,33 @@
 #include "main.h"
-#include <stddef.h>
-
 
 /**
- * to_int -converts a binary number to an unsigned int.
- * @i: pointing to a string of 0 and 1 chars
- *
- * Return:converted number, or 0
+ * binary_to_uint - converts binary to unsigned int
+ * @b: string of binary number
+ * Return: unsigned integer number
  */
-
-unsigned int to_int(int i)
-{
-	int a = 1;
-	unsigned int hold = 1;
-
-	if (i == 0)
-	return (1);
-
-	for (a = 0; a < i; a++)
-	{
-	hold *= 2;
-	}
-
-	return (hold);
-}
-
-
-/**
- * binary_to_uint -converts a binary number to an unsigned int.
- * @b: pointing to a string of 0 and 1 chars
- *
- * Return:converted number, or 0
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int re;
-	int i, diff;
+	unsigned int num = 0;
+	int i = 0, power = 1;
 
-	re = 0;
-	i = 0;
-
-	if (b == NULL)
-	return (0);
+	while (b == NULL)
+		return (0);
 
 	while (b[i])
 	{
-	if ((b[i] != '0' && b[i] != '1'))
-	return (0);
-
-	i++;
+		if (b[i] == '1' || b[i] == '0')
+			i++;
+		else
+			return (0);
 	}
-	i -= 1;
-
-	diff = i;
+	i--;
 
 	while (i >= 0)
 	{
-
-	if (b[i] == '1')
-	re += to_int(diff - i);
-
-	i--;
+		num += power * (b[i] - '0');
+		i--;
+		power *= 2;
 	}
 
-	return (re);
+	return (num);
 }
